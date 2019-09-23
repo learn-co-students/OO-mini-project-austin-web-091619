93bad4807e0c931db3b45c7452081fb84a8dc2cd
Alec Reid <alecreid0093@gmail.com>
2019-09-23 11:21:30 -0500

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,18 @@
+class Ingredient
+    attr_accessor :ingredient_name
+    @@all = []
 
+    def initialize(ingredient_name)
+        @ingredient_name = ingredient_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.most_common_allergen
+        Allergy.all.max_by {|a| a.ingredient.count(a)}
+    end
+
+end
